64631fe90b40fdec6c4aa1d70d8549d273594706
Kcptonls <cmoretti@cs.princeton.edu>
2021-03-29 03:07:27 -0400

    what am i doing

--- symtablehash.c
+++ symtablehash.c
@@ -236,6 +236,9 @@ int SymTable_contains(SymTable_T oSymTable, const char *pcKey) {
     assert(oSymTable != NULL);
     assert(pcKey != NULL);
     hashval = SymTable_hash(pcKey, oSymTable->maxbucket);
+    if (oSymTable->psArray[hashval] = NULL) {
+       return 0;
+    }
     return LinkedList_contains(oSymTable->psArray[hashval], pcKey);
     }
 
