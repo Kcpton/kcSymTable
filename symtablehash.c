237cd3c9404b0989410d551d0acc2ecdfd7f3199
Kcptonls <cmoretti@cs.princeton.edu>
2021-03-29 02:23:00 -0400

    what am i doing

--- symtablehash.c
+++ symtablehash.c
@@ -200,7 +200,7 @@ SymTable_T SymTable_new_help(size_t maxbucket) {
     oSymTable = (SymTable_T) malloc(sizeof(struct SymTable));
     oSymTable->length = 0;
     oSymTable->maxbucket = maxbucket;
-    oSymTable->psArray = (struct Node**) malloc(sizeof(struct LinkedList)
+    oSymTable->psArray = (struct LinkedList*) malloc(sizeof(struct LinkedList)
         * (maxbucket)); 
     return oSymTable;
 }
