5738023726105e72e392f8e5dbafb784143fff3d
Kcptonls <cmoretti@cs.princeton.edu>
2021-03-30 01:48:54 -0400

    style

--- symtablehash.c
+++ symtablehash.c
@@ -268,6 +268,7 @@ int SymTable_put(SymTable_T oSymTable, const char *pcKey,
     }
     /* this if statement contains the resizing of the oSymTable if the
       SymTable length is equal to the maxbucket length */
+   /*
     if (oSymTable->length == oSymTable->maxbucket && oSymTable->bucketnum < 
          sizeof(auBucketCounts)/sizeof(auBucketCounts[0])) {
         LinkedList_T* oldArray = oSymTable->psArray;
@@ -279,7 +280,9 @@ int SymTable_put(SymTable_T oSymTable, const char *pcKey,
         oSymTable->length = 0;
         oSymTable->psArray = (LinkedList_T*) calloc(sizeof(LinkedList_T),
         (oSymTable->maxbucket)); 
+        */
         /* puts all the old bindings in the new Symtable */
+        /*
         while(i < bucketLen) {
             head = NULL;
             if (oSymTable->psArray[i] != NULL) {
@@ -293,6 +296,7 @@ int SymTable_put(SymTable_T oSymTable, const char *pcKey,
         }
         i = 0;
         /* frees all the old linkedlist */
+        /*
         while(i < bucketLen) {
             if (oldArray[i] != NULL) {
             LinkedList_free(oldArray[i]);
@@ -301,6 +305,53 @@ int SymTable_put(SymTable_T oSymTable, const char *pcKey,
         }
         free(oldArray);
     }
+    */
+
+   if (oSymTable->length == oSymTable->maxbucket && oSymTable->length = 
+      auBucketCounts[oSymTable->bucketnum] && oSymTable->bucketnum < 
+         sizeof(auBucketCounts)/sizeof(auBucketCounts[0])) {
+        LinkedList_T* oldArray = oSymTable->psArray;
+        size_t bucketLen;
+        size_t i = 0;
+        struct Node* head;
+        bucketLen = oSymTable->maxbucket;
+        oSymTable->bucketnum += 1;
+        oSymTable->maxbucket = auBucketCounts[oSymTable->bucketnum];
+        oSymTable->psArray = (LinkedList_T*) calloc(sizeof(LinkedList_T),
+        (oSymTable->maxbucket));
+        oSymTable->length = 0;
+        while(i < bucketLen) {
+            head = NULL;
+            if (oldArray[i] != NULL) {
+            head = oSymTable->psArray[i]->psFirst;
+            }
+            while (head != NULL) {
+                SymTable_put(oSymTable, head->pvKey, head->pvItem);
+                head = head->psNext;
+            }
+            i++;
+        }
+        while(i < bucketLen) {
+            if (oldArray[i] != NULL) {
+            LinkedList_free(oldArray[i]);
+            }
+        }
+        free(oldArray);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return output;
     }
 
