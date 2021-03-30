7353e452f5c8f67f7114c9c0ce44f34684d47511
Kcptonls <cmoretti@cs.princeton.edu>
2021-03-29 23:39:58 -0400

    i swear

--- symtablehash.c
+++ symtablehash.c
@@ -202,11 +202,10 @@ static size_t SymTable_hash(const char *pcKey, size_t uBucketCount) {
 SymTable_T SymTable_new(void) {
    SymTable_T oSymTable;
    oSymTable = (SymTable_T) malloc(sizeof(struct SymTable));
-   oSymTable->length = 0;
-   oSymTable->maxbucket = auBucketCounts[0];
-   /*oSymTable->psArray = (LinkedList_T*) calloc(sizeof(LinkedList_T),
+   /* oSymTable->length = 0; */
+   /* oSymTable->maxbucket = auBucketCounts[0]; */
+   oSymTable->psArray = (LinkedList_T*) calloc(sizeof(LinkedList_T),
         (oSymTable->maxbucket)); 
-        */
    oSymTable->bucketnum = 0;
    return oSymTable;
 }
